9ceb53a483bd06353b80fcdbdf803fd4ebb6047c
mastekator <akmiskam@yandex.ru>
2019-12-11 16:46:33 +0300

    авторизация в гугле

--- src/components/AuthorizationBlock/AuthorizationBlock.js
+++ src/components/AuthorizationBlock/AuthorizationBlock.js
@@ -1,10 +1,26 @@
 import React from "react";
 import classes from './AuthorizationBlock.module.css'
+import Button from "../UI/Button/Button";
 
 const AuthorizationBlock = props => {
     return (
         <div className={classes.AuthorizationBlock}>
-            <h1>Авторизация</h1>
+            {props.userName ?
+                <React.Fragment>
+                <h1>Вы вошли как {props.userName}</h1>
+                    <img src={props.userImageUrl} alt=""/>
+                </React.Fragment>
+                :
+                <React.Fragment>
+                    <h1>Авторизация</h1>
+                    <Button
+                        onClick={props.signIn}
+                    >
+                        Войти с Google
+                    </Button>
+                </React.Fragment>
+            }
+
         </div>
     )
 };
--- src/containers/Authorization/Authorization.js
+++ src/containers/Authorization/Authorization.js
@@ -3,11 +3,48 @@ import classes from './Authorization.module.css'
 import AuthorizationBlock from "../../components/AuthorizationBlock/AuthorizationBlock";
 
 class Authorization extends Component {
+    state = {
+        name: null,
+        image: null,
+    };
+
+    componentDidMount() {
+        window.gapi.load('auth2', function () {
+            window.gapi.auth2.init({
+                client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID
+            })
+                .then(() => console.log('init OK'), () => console.log('init ERROR'))
+        })
+    }
+
+    signInHandler = () => {
+        const _authOk = GoogleUser => {
+            console.log('auth OK', GoogleUser);
+            this.setState({
+                name: GoogleUser.getBasicProfile().getName(),
+                image: GoogleUser.getBasicProfile().getImageUrl()
+            });
+        };
+        const _authError = () => console.log('auth ERROR');
+
+        const GoogleAuth = window.gapi.auth2.getAuthInstance();
+        GoogleAuth.signIn(
+            {
+                scope: 'profile email',
+            }
+        )
+            .then(_authOk, _authError);
+    };
+
     render() {
         return (
             <div className={classes.Authorization}>
                 <div className={classes.AuthorizationWrapper}>
-                    <AuthorizationBlock>
+                    <AuthorizationBlock
+                        userName={this.state.name}
+                        userImageUrl={this.state.image}
+                        signIn={this.signInHandler}
+                    >
                     </AuthorizationBlock>
                 </div>
             </div>
