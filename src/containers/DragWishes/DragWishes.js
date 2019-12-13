26589a7f7d9587bd7f0c12b8aadf2c96a9562855
mastekator <akmiskam@yandex.ru>
2019-12-13 13:37:42 +0300

    добавил запоминание по секциям

--- src/containers/DragWishes/DragWishes.js
+++ src/containers/DragWishes/DragWishes.js
@@ -12,7 +12,7 @@ class DragWishes extends Component {
                     sectionId: 1
                 },
                 {
-                    sectionName: 'Секция 1',
+                    sectionName: 'Секция 2',
                     sectionId: 2
                 }
             ],
@@ -42,6 +42,47 @@ class DragWishes extends Component {
     };
 
     onDragEnd = result => {
+        const {destination, source, draggableId} = result;
+
+        if (!destination) {
+            return;
+        }
+
+        if (destination.droppableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
+
+        const sectionOut = this.state.sections.section[Number(source.droppableId) - 1];
+        const tasks = [...this.state.sections.tasks];
+        // tasks.splice(Number(source.index), 1);
+        // tasks.splice(Number(destination.index), 0, Number(draggableId));
+
+        const section = [...this.state.sections.section];
+
+        tasks[Number(draggableId) - 1] = {
+            taskName: this.state.sections.tasks[Number(draggableId) - 1].taskName,
+            taskId: Number(source.index) + 1,
+            sectionId: Number(destination.droppableId),
+        };
+
+        const sections = {
+            section,
+            tasks
+        }
+
+        this.setState({
+            sections: sections
+        })
+
+        console.log('tasks', this.state.sections.tasks)
+
+        // console.log(sectionOut);
+        // console.log(tasks);
+        // console.log(section);
+        console.log(sections);
+        console.log(Number(draggableId));
 
     };
 
