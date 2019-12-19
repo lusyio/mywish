004c7b1015492ce7fc4df52721812732a78e8203
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2019-12-19 10:47:19 +0300

    добавил заглушку для пустого листа в карточкелистов + выбор листа из списка

--- resources/js/components/Auth/ListCard/ListCard.js
+++ resources/js/components/Auth/ListCard/ListCard.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import classes from './ListCard.module.css'
 import WishList from "./WishList/WishList";
+import Button from "../../UI/Button/Button";
 
-const ListCard = props =>
-    (
+const ListCard = props => {
+    let renderList;
+
+    if (props.lists.count !== 0) {
+        renderList =
+            props.lists.items.map((list) => {
+                return list.id === props.selectedList ? (
+                        <WishList
+                            key={list.id}
+                            wishItems={list.wishItems}
+                        />
+                    )
+                    :
+                    null
+            })
+    } else {
+        renderList =
+            <div className={classes.Empty}>
+                <p>У вас еще нет не одного списка</p>
+                <Button>Создать список</Button>
+            </div>
+
+    }
+
+    return (
         <div className={classes.ListCard}>
             <div className={classes.ListCardBody}>
-                {props.lists.items.map((list) => {
-                    return list.id === 2 ? (
-                            <WishList
-                                key={list.id}
-                                wishItems={list.wishItems}
-                            />
-                        )
-                        :
-                        null
-                })}
+                {renderList}
             </div>
         </div>
-    );
+    )
+};
 
 
 export default ListCard
