1de4311daa0965f27bd3969d71c2cb34944adee2
mastekator <akmiskam@yandex.ru>
2019-12-20 16:22:51 +0300

    поправил путь для картинок

--- resources/js/components/Auth/ListCard/WishList/WishItem/WishItem.js
+++ resources/js/components/Auth/ListCard/WishList/WishItem/WishItem.js
@@ -27,7 +27,7 @@ const WishItem = props => {
         renderWishItem =
             <React.Fragment>
                 <div className={classes.WishItem}>
-                    <img src={'https://mywish.su/' + props.picture} alt={props.title}/>
+                    <img src={'mywish.su/public/' + props.picture} alt={props.title}/>
                     <div>
                         <p className={classes.Title}>{props.title}</p>
                         <a href={props.url}>{props.url}</a>
