86334387d2b935730bc65e779c2bdfb80c2ae428
Igor Garifyanov <garifianov.i@gmail.com>
2019-12-26 16:05:28 +0300

    Доработал title и description

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,26 @@ Route::get('/list/{listLink}', function ($listLink) {
     if (is_null($list)) {
         $ogTitle = "MyWish - составь свой список желаний!";
         $ogUrl = "https://mywish.su";
+        $title = 'MyWish - составь свой список желаний!';
+        $description = 'Составь свой виш лист или список желаний в сервисе MyWish';
     } else {
+        $name = $list->user()->value('name');
         $ogTitle = ($list->name == '') ? "Список желаний" : $list->name;
         $ogUrl = "https://mywish.su/list/" . $listLink;
+        $title = 'Виш лист ' . $list->name;
+        $description = 'Список желаний от пользователя ' . $name;
+
     }
-    return view('index', ['ogTitle' => $ogTitle, 'ogUrl' => $ogUrl]);
+    return view('index', ['ogTitle' => $ogTitle, 'ogUrl' => $ogUrl, 'title' => $title, 'description' => $description]);
 });
 
 Route::get('/', function () {
     $ogTitle = "MyWish - составь свой список желаний!";
     $ogUrl = "https://mywish.su";
-    return view('index', ['ogTitle' => $ogTitle, 'ogUrl' => $ogUrl]);
+    $title = 'MyWish - составь свой список желаний!';
+    $description = 'Составь свой виш лист или список желаний в сервисе MyWish';
+
+    return view('index', ['ogTitle' => $ogTitle, 'ogUrl' => $ogUrl, 'title' => $title, 'description' => $description]);
 });
 
 Route::get('/{any}', function () {
